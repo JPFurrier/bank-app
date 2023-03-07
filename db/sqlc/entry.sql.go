b537f4a87902e56f8037f91025d77b1e25e9b8a7
JPFurrier <eujppacheco@gmail.com>
2023-03-06 22:13:29 -0300

    add CRUD entry

--- db/sqlc/entry.sql.go
+++ db/sqlc/entry.sql.go
@@ -35,13 +35,13 @@ func (q *Queries) CreateEntry(ctx context.Context, arg CreateEntryParams) (Entri
 	return i, err
 }
 
-const getAuthor = `-- name: GetAuthor :one
+const getEntry = `-- name: GetEntry :one
 SELECT id, account_id, amount, created_at FROM entries
 WHERE id = $1 LIMIT 1
 `
 
-func (q *Queries) GetAuthor(ctx context.Context, id int64) (Entries, error) {
-	row := q.db.QueryRowContext(ctx, getAuthor, id)
+func (q *Queries) GetEntry(ctx context.Context, id int64) (Entries, error) {
+	row := q.db.QueryRowContext(ctx, getEntry, id)
 	var i Entries
 	err := row.Scan(
 		&i.ID,
@@ -52,7 +52,7 @@ func (q *Queries) GetAuthor(ctx context.Context, id int64) (Entries, error) {
 	return i, err
 }
 
-const listAuthors = `-- name: ListAuthors :many
+const listEntries = `-- name: ListEntries :many
 SELECT id, account_id, amount, created_at FROM entries
 WHERE account_id = $1
 ORDER BY id
@@ -60,14 +60,14 @@ LIMIT $2
 OFFSET $3
 `
 
-type ListAuthorsParams struct {
+type ListEntriesParams struct {
 	AccountID int64 `json:"accountID"`
 	Limit     int32 `json:"limit"`
 	Offset    int32 `json:"offset"`
 }
 
-func (q *Queries) ListAuthors(ctx context.Context, arg ListAuthorsParams) ([]Entries, error) {
-	rows, err := q.db.QueryContext(ctx, listAuthors, arg.AccountID, arg.Limit, arg.Offset)
+func (q *Queries) ListEntries(ctx context.Context, arg ListEntriesParams) ([]Entries, error) {
+	rows, err := q.db.QueryContext(ctx, listEntries, arg.AccountID, arg.Limit, arg.Offset)
 	if err != nil {
 		return nil, err
 	}
